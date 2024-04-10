f41521ac932510221adbc2788f5064992c1fbaf9
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-10 12:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+input1=document.getElementById("status");
+
+
+function change(){
+    console.log(input1);
+    input1.innerHTML='<h1>Entered Metaverse</h1>'
+}
+
+document.getElementById("enterBtn").addEventListener("click",change);
